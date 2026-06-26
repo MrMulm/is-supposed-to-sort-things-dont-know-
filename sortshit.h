998813988278ea87f62f573c18e38bd1572ffcd1
MrMulm <lennerth05@gmail.com>
2026-06-26 16:02:12 +0200

    Add files via upload

---
+++ sortshit.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+namespace sorters {
+	template <typename T>
+	void num_sort(T tarray[], int elements) {
+		T a, b;
+		for (int n = elements; n > 1; n--) {
+			for (int i = 0; i < elements - 1; i++) {
+				if (tarray[i] > tarray[i + 1]) {
+					a = tarray[i];
+					b = tarray[i + 1];
+					tarray[i] = b;
+					tarray[i + 1] = a;
+				}
+			}
+		}
+	}
+}
